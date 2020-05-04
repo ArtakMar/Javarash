49faeca0451bdf62217fd6d0bd94647284a9e539
DinDinDon <artak_mar.ru@mail.ru>
2020-05-04 16:53:10 +0300

    d

---
+++ Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task10.task1020.algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        sort(array);
+
+        System.out.println(array[9]);//9
+        System.out.println(array[10]);//10
+    }
+
+    public static void sort(int[] array) {
+       int a;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                    if(array[i]>array[j]){ // 0 1
+                        a = array[i]; //0
+                        array[i] = array[j];// 1
+                        array[j] = a;
+                    }
+            }
+
+
+        }//напишите тут ваш код
+    }
+}
